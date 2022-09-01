799308a36edabf41110b733d169912f9513e978c
Andres Fernandez <andres.fernanezcaballero@davinci.edu.ar>
2022-09-01 13:09:32 -0300

    limpia app.js file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import {useAuth} from './Hooks/UseAuth';
 import { Mainroutes } from './Routes/Mainroutes';
-import { Link } from 'react-router-dom';
-import Authconsumer from './Hooks/UseAuth'
-import { useFirestore } from './Hooks/useFirestore';
 import { Navbar } from './Components/Navbar'
 
 function App() {
-  const {isSigned, userauth, signedout ,signin}=Authconsumer()
-  const database = useFirestore()
-  //database.createPartido('partido').then()
-  database.getAllPartidos()
-    .then(partidos => partidos.forEach(partido => console.log(partido)))
   return (
     <div >
       <Navbar></Navbar>
