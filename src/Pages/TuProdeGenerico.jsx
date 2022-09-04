bdd3e4f1826c0db3c976505053eb640d9d6c677d
Nicolas <Nicolascruces0@gmail.com>
2022-09-04 20:41:01 -0300

    octavos no funcionando

--- src/Pages/TuProdeGenerico.jsx
+++ src/Pages/TuProdeGenerico.jsx
@@ -11,8 +11,18 @@ export const TuProdeGenerico = (props) => {
   const onSubmitFinalStrategy = props.onSubmitFinalStrategy
   const validarcamposvacios = props.validarcamposvacios
   const [resultados, setresultados] = useState([])
-
-
+  const [octavos, setoctavos] = useState(
+    { 
+      a: { a: '', b: '' }, 
+      b: { a: '', b: '' }, 
+      c: { a: '', b: '' }, 
+      d: { a: '', b: '' }, 
+      e: { a: '', b: '' },
+      f: { a: '', b: '' },
+      g: { a: '', b: '' },
+      h: { a: '', b: '' } 
+    })
+    console.log('octa',octavos)
   const [inputbracket, setinputbracket] = useState({})
   const [torneo, settorneo] = useState({
     cuartos: { a: { a: '', b: '' }, b: { a: '', b: '' }, c: { a: '', b: '' }, d: { a: '', b: '' } },
@@ -25,19 +35,21 @@ export const TuProdeGenerico = (props) => {
   
   const auth = AuthConsumer();
   const firestore = useFirestore()
+
   const cargarprode = async () => {
+
     if(auth.userauth.uid){
       const prodeusuario = await firestore.getprodeporid(auth.userauth.uid)
         console.log(prodeusuario,'prodeusuario')
         settorneo(prodeusuario.torneo)
         setresultados([...prodeusuario.resultados])
-        
+        setoctavos(prodeusuario.octavos)
+        console.log('octavos en efecto',octavos)
       }
   }
   useEffect(()=>{
    cargarprode().then() 
-   console.log('resultados', resultados)
-   console.log('tor',torneo)
+   
   },[])
 
   const handleOnSubmit = (evento) => {
@@ -55,7 +67,8 @@ export const TuProdeGenerico = (props) => {
       const prodeusuario = {
         resultados, torneo, userid: auth.userauth.uid
       }
-      onSubmitFinalStrategy(prodeusuario.userid, { resultados: prodeusuario.resultados, torneo: prodeusuario.torneo })
+      console.log('octa',octavos)
+      onSubmitFinalStrategy(prodeusuario.userid, { resultados: prodeusuario.resultados,octavos, torneo: prodeusuario.torneo })
     })
       .catch(error => {
         alert('No se pudo guardar el prode intente mas tarde')
@@ -73,11 +86,31 @@ export const TuProdeGenerico = (props) => {
     }
 
   }
-  const handleOnChangeBracket = (evento) => {
-    setinputbracket({ ...inputbracket, [evento.target.name]: evento.target.value })
+  
+  const handleOnChangeOctavos = (evento) => {
+    const octavosauxiliar={...octavos}
+    switch (evento.target.name){
+      case '1-a': octavosauxiliar.a.a=evento.target.value; break ;
+      case '2-a': octavosauxiliar.a.b=evento.target.value; break ;
+      case '1-b': octavosauxiliar.b.a=evento.target.value; break ;
+      case '2-b': octavosauxiliar.b.b=evento.target.value; break ;
+      case '1-c': octavosauxiliar.c.a=evento.target.value; break ;
+      case '2-c': octavosauxiliar.c.b=evento.target.value; break ;
+      case '1-d': octavosauxiliar.d.a=evento.target.value; break ;
+      case '2-d': octavosauxiliar.d.b=evento.target.value; break ;
+      case '1-e': octavosauxiliar.e.a=evento.target.value; break ;
+      case '2-e': octavosauxiliar.e.b=evento.target.value; break ;
+      case '1-f': octavosauxiliar.f.a=evento.target.value; break ;
+      case '2-f': octavosauxiliar.f.b=evento.target.value; break ;
+      case '1-g': octavosauxiliar.g.a=evento.target.value; break ;
+      case '2-g': octavosauxiliar.g.b=evento.target.value; break ;
+      case '1-h': octavosauxiliar.h.a=evento.target.value; break ;
+      case '2-h': octavosauxiliar.h.b=evento.target.value; break ;
+    }
+    setoctavos(octavosauxiliar)
+    
 
   }
-
 
 
 
@@ -119,99 +152,129 @@ export const TuProdeGenerico = (props) => {
 
         <div>
           <label>1ro Grupo A :</label>
-          <select onChange={handleOnChangeBracket} name='1-a'>
-            {torneo.cuartos.a.a!=''?
-            <option value={torneo.cuartos.a.a}>{torneo.cuartos.a.a}</option>:
+          <select onChange={handleOnChangeOctavos} name='1-a'>
+            {octavos.a.a!=''  ?
+            <option value={octavos.a.a}>{octavos.a.a}</option>:
             <option value=''>seleccione un pais</option>}
             {Object.keys(grupoA).map(equipo => <option value={equipo} >{equipo}</option>)}
           </select>
           <label> 2do Grupo A :</label>
-          <select onChange={handleOnChangeBracket} name='2-a'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='2-a'>
+          {octavos.a.b!='' ?
+            <option value={octavos.a.b}>{octavos.a.b}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoA).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <br></br>
           <br></br>
           <label>1ro Grupo B :</label>
-          <select onChange={handleOnChangeBracket} name='1-b'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='1-b'>
+          {octavos.b.a!='' ?
+            <option value={octavos.b.a}>{octavos.b.a}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoB).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <label> 2do Grupo B :</label>
-          <select onChange={handleOnChangeBracket} name='2-b'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='2-b'>
+          {octavos.b.b!='' ?
+            <option value={octavos.b.b}>{octavos.b.b}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoB).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <br></br>
           <br></br>
           <label>1ro Grupo C :</label>
-          <select onChange={handleOnChangeBracket} name='1-c'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='1-c'>
+          {octavos.c.a!='' ?
+            <option value={octavos.c.a}>{octavos.c.a}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoC).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <label> 2do Grupo C :</label>
-          <select onChange={handleOnChangeBracket} name='2-c'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='2-c'>
+          {octavos.c.b!=''?
+            <option value={octavos.c.b}>{octavos.c.b}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoC).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <br></br>
           <br></br>
           <label>1ro Grupo D :</label>
-          <select onChange={handleOnChangeBracket} name='1-d'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='1-d'>
+          {octavos.d.a!=''  ?
+            <option value={octavos.d.a}>{octavos.d.a}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoD).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <label> 2do Grupo D :</label>
-          <select onChange={handleOnChangeBracket} name='2-d'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='2-d'>
+          {octavos.d.b!=''?
+            <option value={octavos.d.b}>{octavos.d.b}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoD).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <br></br>
           <br></br>
           <label>1ro Grupo E :</label>
-          <select onChange={handleOnChangeBracket} name='1-e'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='1-e'>
+          {octavos.e.a!='' ?
+            <option value={octavos.e.a}>{octavos.e.a}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoE).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <label> 2do Grupo E :</label>
-          <select onChange={handleOnChangeBracket} name='2-e'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='2-e'>
+          {octavos.e.b!='' ?
+            <option value={octavos.e.b}>{octavos.e.b}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoE).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <br></br>
           <br></br>
           <label>1ro Grupo F :</label>
-          <select onChange={handleOnChangeBracket} name='1-f'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='1-f'>
+          {octavos.f.a!='' ?
+            <option value={octavos.f.a}>{octavos.f.a}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoF).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <label> 2do Grupo F :</label>
-          <select onChange={handleOnChangeBracket} name='2-f'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='2-f'>
+          {octavos.f.b!='' ?
+            <option value={octavos.f.b}>{octavos.f.b}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoF).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <br></br>
           <br></br>
           <label>1ro Grupo G :</label>
-          <select onChange={handleOnChangeBracket} name='1-g'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='1-g'>
+          {octavos.g.a!=''?
+            <option value={octavos.g.a}>{octavos.g.a}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoG).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <label> 2do Grupo G :</label>
-          <select onChange={handleOnChangeBracket} name='2-g'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='2-g'>
+          {octavos.g.b!='' ?
+            <option value={octavos.g.b}>{octavos.g.b}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoG).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <br></br>
           <br></br>
           <label>1ro Grupo H :</label>
-          <select onChange={handleOnChangeBracket} name='1-h'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='1-h'>
+          {octavos.h.a!='' ?
+            <option value={octavos.h.a}>{octavos.h.a}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoH).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <label> 2do Grupo H :</label>
-          <select onChange={handleOnChangeBracket} name='2-h'>
-            <option value=''>seleccione un pais</option>
+          <select onChange={handleOnChangeOctavos} name='2-h'>
+          {octavos.h.b!='' ?
+            <option value={octavos.h.b}>{octavos.h.b}</option>:
+            <option value=''>seleccione un pais</option>}
             {Object.keys(grupoH).map(equipo => <option value={equipo}>{equipo}</option>)}
           </select>
           <br></br>
