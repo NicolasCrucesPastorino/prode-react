7299c456dfec3ca9ce9b7abdf0326d744163178d
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2022-09-04 17:00:38 -0300

    MODIFICA ASPECTO VISUAL DE CODIGO Y VISTA

--- src/Pages/FormularioLoginTuProde.jsx
+++ src/Pages/FormularioLoginTuProde.jsx
@@ -1,33 +1,39 @@
 import React,{useState} from 'react'
 import authconsumer from './../Hooks/UseAuth'
-import { useFirestore } from '../Hooks/useFirestore'
+
+
 export const FormularioLoginTuProde = () => {
     const [email, setemail] = useState('')
     const [password, setpassword] = useState('')
     const consumer = authconsumer()
-    const firestore = useFirestore()
+
     const handleOnSubmit = (evento) => {
         evento.preventDefault()
-    consumer.logearseEnTuProde(email,password).then(response => console.log('autenticado'))
-    .catch(error => console.log('error',error))
+        consumer.logearseEnTuProde(email,password)
+            .then(() => console.log('autenticado'))
+            .catch(error => console.log('error', error)
+        );
     }
 
     const handleOnChangeEmail = (evento) => {
         setemail(evento.target.value)
     }
+
     const handleOnChangePassword = (evento) => {
         setpassword(evento.target.value)
     }
-  return (
-    <div>
-       
-        <form onSubmit={handleOnSubmit}>
-            <label className='form-label'>Email:</label>
-            <input className='form-control' type="email" name='email'  onChange={handleOnChangeEmail}></input>
-            <label className='form-label'>Contraseña:</label>
-            <input className='form-control' type="password" name='password' onChange={handleOnChangePassword}></input>
-            <button type='submit'>Log-in</button>
-        </form>
-    </div>
-  )
+
+    return (
+        <div className='container m-4'>
+            <h1>Login</h1>
+            <hr />
+            <form onSubmit={handleOnSubmit} className='m-4'>
+                <label className='form-label'>Email:</label>
+                <input className='form-control' type="email" name='email'  onChange={handleOnChangeEmail}></input>
+                <label className='form-label'>Contraseña:</label>
+                <input className='form-control' type="password" name='password' onChange={handleOnChangePassword}></input>
+                <button type='submit' className='btn btn-primary mt-2'>Log-in</button>
+            </form>
+        </div>
+    )
 }
