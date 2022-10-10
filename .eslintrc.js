0b4ea4ad66279c7c005999de8d3de119c6578886
Nicolas <Nicolascruces0@gmail.com>
2022-10-10 20:38:42 -0300

    agrega eslint y prettier

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "standard",
+        "eslint-config-prettier"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
