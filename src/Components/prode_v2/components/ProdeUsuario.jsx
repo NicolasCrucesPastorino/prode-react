890e775f1db86bcd227397ce3ecd9c0ef2961f47
Andres Fernandez <andres.fernanezcaballero@davinci.edu.ar>
2022-10-17 14:00:16 -0300

    MODIFY logic

--- src/Components/prode_v2/components/ProdeUsuario.jsx
+++ src/Components/prode_v2/components/ProdeUsuario.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Preliminares from "../parts/Preliminares";
 import ConsumerProdeContext, { ProdeProvider } from "../ProdeProvider";
+import {getprodeporid, storeUserProde} from './../../../database/services/prodeService'
 
 const ProdeUsuario = ({ uid, isEditable = true }) => {
   const prode = ConsumerProdeContext();
-  console.log(prode);
 
   const handleOnClick = () => {
     alert("hola");
@@ -21,9 +21,43 @@ const ProdeUsuario = ({ uid, isEditable = true }) => {
   );
 };
 
-const ProdeUsuarioWithContext = (props) => (
-  <ProdeProvider>
-    <ProdeUsuario />
-  </ProdeProvider>
-);
-export default ProdeUsuarioWithContext;
+const ProdeUsuarioWithContext = ({uid}) => {
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [globalProde, setGlobalProde] = useState({})
+  
+  useEffect(
+    () => {
+      const getProdeById = async (uid='') => {
+        console.log('id', uid)
+        const prode = await getprodeporid(uid)
+        console.log('prode', prode);
+        try {
+          setIsLoading(false)
+          setGlobalProde(prode)
+          setIsLoading(true)
+        }catch (error) {
+          console.error(error)
+          setIsLoading(false)
+          throw new Error(error)
+      }
+    }
+    getProdeById(uid)
+      .then(() => {
+        console.log('Prode cargado')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+
+    },[])
+    
+  return(
+      <>
+        <h1>prode usuario id: {uid}</h1>
+        <p>{globalProde.resultados?<span>valor { globalProde.resultados[0].partidos[0].partidoid}</span> : <span>sin resultados</span>}</p>
+      </>
+  )
+}
+
+export default ProdeUsuarioWithContext
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { ProdeUsuario } from '../Components/prode_v2/components/ProdeUsuario'
+import ProdeUsuario from '../Components/prode_v2/components/ProdeUsuario'
 import AuthConsumer from './../Hooks/UseAuth'
 
 export const Home = () => {
 
-  const {signedout} = AuthConsumer()
+  const {signedout, userauth} = AuthConsumer()
   return (
     <div>
       <h1>Home</h1>
       <section>
         <h2>{process.env.REACT_APP_CANTIDAD_USUARIOS} Personas inscriptas</h2>
-        <ProdeUsuario />
+        <ProdeUsuario uid={userauth.uid} />
       </section>
       <button onClick={signedout} className='btn btn-danger'>Log Out</button>
     </div>
