e14541a1fce81e2b5e3da9e7bf17b646e03ebdee
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2022-10-28 15:57:08 -0300

    Agrega fecha limite para edicion de prode

--- src/Pages/TuProde.jsx
+++ src/Pages/TuProde.jsx
@@ -1,16 +1,20 @@
 import ProdeUsuario from "../Components/prode_v2/components/ProdeUsuario";
 import AuthConsumer from "../Hooks/UseAuth";
+import { daysLeftToCloseProde } from "../utils/dateUtils";
 
 export const TuProde = () => {
   const { userauth } = AuthConsumer();
-  const fechaLimite = '2022-11-20:00:00:000Z' //'2021-06-11T00:00:00.000Z';
-  const isEditable = (Date(fechaLimite) - Date.now() > 0)
+  const diasHataCerrarProde = daysLeftToCloseProde();
+  const isEditable = diasHataCerrarProde > 0;
 
   return (
     <section>
+      <header>
       <h1>
         Prode de <span>{userauth.displayName}</span>
       </h1>
+      <p>Dias restantes para Jugar {diasHataCerrarProde}</p>
+      </header>
       
       <ProdeUsuario uid={userauth.uid} isEditable={isEditable} />
     </section>
